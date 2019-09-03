8be3b31e0eb8e344c23cf459c28489cfd7d1db25
PinsardF <mysteries@orange.fr>
2019-09-03 22:45:09 +0200

    Textbox working. Objective : list of spots

--- app/src/main/java/com/example/cheapquest/Combat.java
+++ app/src/main/java/com/example/cheapquest/Combat.java
@@ -2,6 +2,7 @@ package com.example.cheapquest;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,8 @@ public class Combat extends AppCompatActivity {
         setContentView(R.layout.activity_combat);
         load_characters();
         load_infos();
+        TextView textBox = findViewById(R.id.textbox);
+        textBox.setText("Un "+ennemy.getName()+" apparaît !");
     }
 
     private void load_infos() {
@@ -44,34 +47,53 @@ public class Combat extends AppCompatActivity {
     }
 
     private void load_characters() {
-        ennemy = new Character("Monstre", 3, 10,getDrawable(R.drawable.hero));
-        hero = new Character("Vous",5,50,getDrawable(R.drawable.monster));
+        ennemy = new Character("Monstre", 3, 10,getDrawable(R.drawable.tiny_hero));
+        hero = new Character("Vous",5,50,getDrawable(R.drawable.tiny_monster));
     }
 
     public void attack(View view) {
+        TextView textBox = findViewById(R.id.textbox);
+
         int new_ennemy_health = ennemy.getHealth()-hero.getAttack();
         if(new_ennemy_health <= 0){
             new_ennemy_health = 0;
         }
         ennemy.setHealth(new_ennemy_health);
+        textBox.setText("Vous infligez "+hero.getAttack()+" dégats au "+ennemy.getName()+".");
         TextView ennemy_health = findViewById(R.id.ennemy_health);
         if(ennemy.health > 0) {
-            ennemy_health.setText(ennemy.health + "/" + ennemy.max_health + " PV");
-            ennemy_attack();
+            ennemy_health.setText(ennemy.getHealth() + "/" + ennemy.getMax_health() + " PV");
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    ennemy_attack();
+                }
+            }, 1000);
         }else{
-            ennemy_death();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    ennemy_death();
+                }
+            }, 1000);
         }
     }
 
     private void ennemy_death() {
+        TextView textBox = findViewById(R.id.textbox);
+
         TextView ennemy_health = findViewById(R.id.ennemy_health);
         ennemy_health.setText("Mort");
+        textBox.setText("Vous avez vaincu le "+ennemy.getName()+" !");
     }
 
     private void ennemy_attack() {
+        TextView textBox = findViewById(R.id.textbox);
+
         int new_hero_health = hero.getHealth()-ennemy.getAttack();
         hero.setHealth(new_hero_health);
         TextView hero_health = findViewById(R.id.hero_health);
-        hero_health.setText(hero.health+"/"+hero.max_health+" PV");
+        hero_health.setText(hero.getHealth()+"/"+hero.getMax_health()+" PV");
+        textBox.setText("Le "+ennemy.getName()+" vous a infligé "+ennemy.getAttack()+" dégats.");
     }
 }
