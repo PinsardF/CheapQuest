633e5ee4661470bf45be65d297545e1944e99935
PinsardF <mysteries@orange.fr>
2019-09-04 15:37:45 +0200

    Quit kinda works. Objective : xp system

--- app/src/main/java/com/example/cheapquest/MapActivity.java
+++ app/src/main/java/com/example/cheapquest/MapActivity.java
@@ -22,7 +22,8 @@ public class MapActivity extends AppCompatActivity {
     }
 
     public void go_to_village(){
-        finish();
-        System.exit(0);
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 }
